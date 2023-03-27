b190351b7cb29226204e1073eac4a7255e05e429
romanamo <r-schoene@gmx.net>
2023-03-27 23:06:06 +0200

    licensing and better readme added

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-Small tool to explore fractals
+<div align="center">
+  <a href="https://github.com/romanamo/fractolio">
+    <img src="assets/logo-round.png" alt="Logo" width="128" height="128">
+  </a>
+    <h3 align="center">fractolio</h3>
+</div>
 
-<img src="assets/logo.png"  width="300" height="300">
+## About The Project
+
+**fractolio** is supposed to be a small, free, efficient and highly customizable tool
+to explore 2 dimensional fractals.
+Such as colorful visualized Plots of Julia Sets, of the Mandelbrot Set in special and many more to come.
+The Creation of this tool started with the motivation to learn more about efficient computing and the visualisation
+of fractals.
+
+## Built with
+
+* Apfloat
+* JavaFx
+* Maven
+
+## Getting started
+
+soon to be added
+
+## Installation
+
+soon to be added
+
+## License
+
+Distributed under the MIT License. See `LICENSE.md` for more information.
--- src/main/java/de/romanamo/fractolio/Main.java
+++ src/main/java/de/romanamo/fractolio/Main.java
@@ -1,7 +1,6 @@
 package de.romanamo.fractolio;
 
-import de.romanamo.fractolio.model.color.HueMap;
-import de.romanamo.fractolio.model.color.LightMap;
+import de.romanamo.fractolio.model.color.BlackWhiteMap;
 import de.romanamo.fractolio.model.draw.ImageDrawer;
 import de.romanamo.fractolio.model.draw.ImageSize;
 import de.romanamo.fractolio.model.evaluator.IterationalSetEvaluator;
@@ -16,10 +15,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SetEvaluator evaluator = new IterationalSetEvaluator(15, new Apfloat(2), new EuclideanMetric());
+        SetEvaluator evaluator = new IterationalSetEvaluator(10, new Apfloat(2), new EuclideanMetric());
 
         ComplexFunction func = new QuadraticPolynomialFunction(new Apcomplex(new Apfloat(-0.70176), new Apfloat(-0.3842)));
-        ImageDrawer drawer = new ImageDrawer(func, new LightMap(1, 0, 0), evaluator, new ImageSize(300,300));
+        ImageDrawer drawer = new ImageDrawer(func, new BlackWhiteMap(), evaluator, new ImageSize(300,300));
         drawer.draw();
     }
 }
